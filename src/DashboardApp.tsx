a953d6c0a0a125f594aa2aafdb732c937078e21e
André Holdschick <andre.holdschick@googlemail.com>
2025-11-06 15:49:23 +0100

    Avoid recreating mark fetch callback on mark updates

--- src/DashboardApp.tsx
+++ src/DashboardApp.tsx
@@ -464,7 +464,7 @@ export default function DashboardApp({ onOpenPlaybookIndex }: DashboardAppProps 
     }
 
     setMarkFetch(prev => ({ ...prev, inProgress: false }));
-  }, [legMarks]);
+  }, [setLegMarks, setMarkFetch]);
 
   function Spinner({ className = "h-4 w-4" }: { className?: string }) {
     return (
